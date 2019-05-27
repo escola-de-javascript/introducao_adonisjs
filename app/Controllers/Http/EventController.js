17a4ebf8083802dc2be87fa7474149ab5aa11632
Daniel Moreto <dfmoreto@gmail.com>
2019-05-27 16:51:25 -0300

    Update :index and add :create and :store actions
    Event loading was added on :index action and new actions :create and :strore was created

--- app/Controllers/Http/EventController.js
+++ app/Controllers/Http/EventController.js
@@ -1,8 +1,21 @@
 'use strict'
 
+const Event = use('App/Models/Event');
+
 class EventController {
-  index({ view }) {
-    return view.render('event.index')
+  async index({ view }) {
+    const events = await Event.all()
+    return view.render('event.index', { events: events.toJSON() })
+  }
+
+  create({ view }) {
+    return view.render('event.create')
+  }
+
+  async store({ request, response }) {
+    const eventParams = request.only(['title', 'date', 'description'])
+    await Event.create({ ...eventParams })
+    return response.route('events.index')
   }
 }
 
