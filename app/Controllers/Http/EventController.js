8e03f81d7ad0563a68d9d33d98dbc433345b38ab
Daniel Moreto <dfmoreto@gmail.com>
2019-05-27 21:49:32 -0300

    Add action and route to delete Event

--- app/Controllers/Http/EventController.js
+++ app/Controllers/Http/EventController.js
@@ -30,6 +30,12 @@ class EventController {
     await event.save()
     return response.route('events.index')
   }
+
+  async destroy({ params, response }) {
+    const event = await Event.find(params.id)
+    await event.delete()
+    return response.route('events.index')
+  }
 }
 
 module.exports = EventController
